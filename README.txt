d66c0363796a06ee682c6f3bc40137f0cac0143b
Joe <joe@joetoday.com>
2014-11-13 16:42:52 -0800

    first commit

---
+++ README.txt
@@ -0,0 +1,6 @@
+A custom module to enhance the wysiwyg experience
+
+
+ckeditor-config.js
+------------------
+custom configurations for CKeditor
---
+++ ckeditor-config.js
@@ -0,0 +1,13 @@
+// see ckeditor/_source/core/config.js for configuration options
+
+CKEDITOR.editorConfig = function( config ) {
+
+  // Define changes to default configuration here.
+  
+  //Adds node class to wysiwyg editor field so it inherits node styling
+  config.bodyClass = 'node';
+
+  // allow i tags to be empty (for font awesome)
+  config.dtd.$removeEmpty['i'] = false
+  
+}
